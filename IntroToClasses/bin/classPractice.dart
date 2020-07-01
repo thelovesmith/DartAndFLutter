6d6ea7904f600579066549c87c1d0aed91835398
thelovesmith <averydante9@gmail.com>
2020-06-30 22:39:17 -0400

    stared class

---
+++ IntroToClasses/bin/classPractice.dart
@@ -0,0 +1,15 @@
+class Date {
+  String name;
+  int age;
+  bool orgasm;
+  bool protected;
+  String sex;
+  String sun;
+  String moon;
+  String rising;
+  String notes; 
+}
+
+main(List<String> args) {
+  print("I am Avery");
+}
