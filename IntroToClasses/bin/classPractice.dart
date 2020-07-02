d2ffc33c93b1005ac9e52426405050eadddef066
thelovesmith <averydante9@gmail.com>
2020-07-01 21:46:15 -0400

    classes

--- IntroToClasses/bin/classPractice.dart
+++ IntroToClasses/bin/classPractice.dart
@@ -8,15 +8,20 @@ class Date {
   String sun;
   String moon;
   String rising;
-  String notes;
+  String notes = "She enjoys happy hour and byob dates"; //initialize this variable 
 }
 
 main(List<String> args) {
   print("I am Avery");
 
-  var date = new Date(); // here we actually create an instance of the object
+  var date = new Date(); // here we actually create an instance of an object with the type Date
+
   date.name = "Jenny";
   date.age = 26;
-  print(date.name);
-  print(date);
+  date.notes = "She enjoys happy hour and korean bbq";
+  date.sex = "female";
+
+  print(date.name); // dot access operator
+  print(date.notes);
+  print(date.sex);
 }
