63d826118be66bb98f28f1488cb2b18f671639c8
thelovesmith <averydante9@gmail.com>
2020-07-03 05:46:41 -0400

    started constructors

--- IntroToClasses/bin/classPractice.dart
+++ IntroToClasses/bin/classPractice.dart
@@ -1,5 +1,6 @@
+//! creating the nblue print for a class object
 class Date {
-  // Instance variables and ember variables
+  //! Instance variables and ember variables
   String name;
   int age;
   bool orgasm;
@@ -11,7 +12,21 @@ class Date {
   String notes =
       "She enjoys happy hour and byob dates"; //initialize this variable
 
-  // Methods or functions
+// ! Constructors
+// explicit constructor //
+  Date(String name, int age, bool orgasm, bool protected, String sex,
+      String sun, String moon, String rising, String notes) {
+    this.name = name;
+    this.age = age;
+    this.orgasm = orgasm;
+    this.protected = protected;
+    this.sex = sex;
+    this.notes = notes;
+    this.sun = sun;
+    this.moon = moon;
+    this.rising = rising;
+  }
+  //! Methods or functions
 
   void printName() {
     print('$name');
@@ -29,13 +44,14 @@ main(List<String> args) {
   // print("I am Avery");
 
   var date =
-      new Date(); // here we actually create an instance of an object with the type Date
+      //new Date(); // here we actually create an instance of an object with the type Date
 
-  date.name = "Jenny";
+      new Date("Jenny", 27, true, false, "Female",
+          "She enjoys talkign about work", "Leo", "Pisces", "gemini");
+  // date.name = "Jenny";
   // date.age = 26;
-  date.notes = "She enjoys happy hour and korean bbq";
+  // date.notes = "She enjoys happy hour and korean bbq";
   // date.sex = "female";
-
   // print(date.name); // dot access operator
   // print(date.notes);
   // print(date.sex);
