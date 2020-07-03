94066925e4216405bf23cd95db7dfcb292216f35
thelovesmith <averydante9@gmail.com>
2020-07-03 05:36:58 -0400

    intro to methods

--- IntroToClasses/bin/classPractice.dart
+++ IntroToClasses/bin/classPractice.dart
@@ -8,20 +8,37 @@ class Date {
   String sun;
   String moon;
   String rising;
-  String notes = "She enjoys happy hour and byob dates"; //initialize this variable 
+  String notes =
+      "She enjoys happy hour and byob dates"; //initialize this variable
+
+  // Methods or functions
+
+  void printName() {
+    print('$name');
+  }
+
+  // void setSex() {}
+  bool isOrgasm() {
+    return true;
+  }
+
+  String createNotes() => "$notes";
 }
 
 main(List<String> args) {
-  print("I am Avery");
+  // print("I am Avery");
 
-  var date = new Date(); // here we actually create an instance of an object with the type Date
+  var date =
+      new Date(); // here we actually create an instance of an object with the type Date
 
   date.name = "Jenny";
-  date.age = 26;
+  // date.age = 26;
   date.notes = "She enjoys happy hour and korean bbq";
-  date.sex = "female";
+  // date.sex = "female";
 
-  print(date.name); // dot access operator
-  print(date.notes);
-  print(date.sex);
+  // print(date.name); // dot access operator
+  // print(date.notes);
+  // print(date.sex);
+  date.printName();
+  print(date.createNotes());
 }
