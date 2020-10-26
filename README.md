bcdc6fe892f7ddec970826f9192f4838c2c819fe
Avery-Dante Hinds <averydante9@gmail.com>
2020-10-26 16:27:58 -0400

    working

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 
 Dart &amp; Flutter
 
-Dart is an Object Oriented LAnguage that has a Plethora of use cases including the Framework Flutter which is used to create Android and Iphone
+Dart is an Object Oriented LAnguage that has a Plethora of use cases including the Framework Flutter which we use to create Android and Iphone applications
 
 ## Dart Styling Guide
 
 [Style Guid][1]
 [1]:https://www.dartlang.org/guides/language/effective-dart/style
+
+
+
+
+Useful Flutter Widgets: 
++ Inkwell
++ GestureDetector 
+
+
+FLutter Reasearch: 
++ throw UnimplementedError();
--- firstFlutterApp/lib/ui/home.dart
+++ firstFlutterApp/lib/ui/home.dart
@@ -29,29 +29,31 @@ class ScaffoldExample extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            InkWell(
-              focusColor: Colors.purpleAccent,
-              highlightColor: Colors.amber,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              child: Container(
-                decoration: new BoxDecoration(
-                  boxShadow: [
-                    new BoxShadow(
-                      color: Colors.amberAccent.shade200,
-                      offset: new Offset(5.0, 5.0),
-                      blurRadius: 5.5,
-                    )
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("TAP ME!",
-                      style:
-                          TextStyle(fontStyle: FontStyle.italic, fontSize: 20)),
-                ),
-              ),
-              onTap: () => debugPrint("Ink Well Tapped"),
-            )
+            CustomButton()
+            // InkWell(
+            //   focusColor: Colors.purpleAccent,
+            //   highlightColor: Colors.amber,
+            //   borderRadius: BorderRadius.all(Radius.circular(25)),
+            //   child: Container(
+            //     // decoration: new BoxDecoration(
+            //     //   boxShadow: [
+            //     //     new BoxShadow(
+            //     //       color: Colors.amberAccent.shade200,
+            //     //       offset: new Offset(5.0, 5.0),
+            //     //       blurRadius: 5.5,
+            //     //     )
+            //     //   ],
+            //     // ),
+            //     child: Padding(
+            //       padding: const EdgeInsets.all(8.0),
+            //       child: Text("TAP ME!",
+            //           style:
+            //               TextStyle(fontStyle: FontStyle.italic, fontSize: 20)),
+            //     ),
+            //   ),
+            //   onTap: () => debugPrint("Ink Well Tapped"),
+            //   onLongPress: () => debugPrint("Long PRess"),
+            // )
           ],
         ),
       ),
@@ -75,5 +77,27 @@ class Home extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          final snackBar = SnackBar(content: Text("one tap"));
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        onDoubleTap: () {
+          final snackBar2 = SnackBar(content: Text("double tap"));
+          Scaffold.of(context).showSnackBar(snackBar2);
+        },
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              color: Colors.pinkAccent,
+              borderRadius: BorderRadius.circular(8.0)),
+          child: Text("Button"),
+        ));
   }
 }
