bc9696d98e44daa86f3c2de917d2d49bd8aa16cf
Avery-Dante Hinds <averydante9@gmail.com>
2020-11-23 15:42:24 -0500

    Wisdom app

---
+++ firstFlutterApp/lib/ui/quotesApp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+//! Stateful widget
+class Wisdom extends StatefulWidget {
+  @override
+  _WisdomState createState() => _WisdomState();
+}
+
+//! State object
+class _WisdomState extends State<Wisdom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
