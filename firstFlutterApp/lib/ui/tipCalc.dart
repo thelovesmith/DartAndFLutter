83e2104bd1df7b722db0eb1dd9c1b485c6c40f66
Avery-Dante Hinds <averydante9@gmail.com>
2020-12-09 15:06:31 -0500

    started tip calculator

--- firstFlutterApp/lib/ui/tipCalc.dart
+++ firstFlutterApp/lib/ui/tipCalc.dart
@@ -1,3 +1,4 @@
+import 'package:firstFlutterApp/utils/hexColors.dart';
 import 'package:flutter/material.dart';
 
 class TipCalculator extends StatefulWidget {
@@ -9,6 +10,7 @@ class _CalculatorState extends State<TipCalculator> {
   int _tipPercentage = 0;
   double _billAmount = 0.0;
   int _personCounter = 1;
+  Color _orange = HexColor("#f9cf90");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,21 +25,56 @@ class _CalculatorState extends State<TipCalculator> {
             Container(
               width: 150,
               height: 200,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Text("Total Per Person"), Text("\$123")],
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("Total Per Person"), Text("\$123")],
+                ),
               ),
               decoration: BoxDecoration(
-                  color: Colors.purple.shade50,
-                  borderRadius: BorderRadius.circular(25.0)),
+                  color: _orange, borderRadius: BorderRadius.circular(25.0)),
             ),
             Container(
-              margin: EdgeInsets.only(top: 10),
+              margin: EdgeInsets.only(top: 15),
+              padding: EdgeInsets.all(12),
               width: 150,
-              height: 100,
+              height: 300,
               decoration: BoxDecoration(
-                  color: Colors.purple.shade50,
-                  borderRadius: BorderRadius.circular(25.0)),
+                color: Colors.transparent,
+                border: Border.all(
+                    color: Colors.blueGrey.shade100,
+                    width: 1,
+                    style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    decoration: InputDecoration(
+                        labelText: "Bill Amount",
+                        prefixIcon: Icon(Icons.attach_money)),
+                    onChanged: (String value) {
+                      try {
+                        _billAmount = double.parse(
+                            value); //have to parse the string input into a double because the bill amount was instantiated as a double
+                      } catch (exception) {
+                        //in case aything happens the bill amount will show as 0.0
+                        _billAmount = 0.0;
+                      }
+                    },
+                  ),
+                  Divider(
+                    thickness: 2.0,
+                  ),
+                  Row(),
+                ],
+              ),
             ),
           ],
         ),
---
+++ firstFlutterApp/lib/utils/hexColors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//! Class for converting hex value colors int something we can use in Flutter
+//Flutter uses a different format for colors
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+//How to use:
+/*
+!Instantiate these in your classes and you can use them anywhere in that widget
+  Color color1 = HexColor("b74093");
+Color color2 = HexColor("#b74093");
+Color color3 = HexColor("#88b74093"); // if you wish to use ARGB format
+ */
