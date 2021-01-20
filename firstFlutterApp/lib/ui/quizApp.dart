037496768e98feee22bd435e87c05fab572795f8
thelovesmith <averydante9@gmail.com>
2021-01-20 13:07:25 -0500

    continued creatng app ui and button logic for parsing through question list

--- firstFlutterApp/lib/ui/quizApp.dart
+++ firstFlutterApp/lib/ui/quizApp.dart
@@ -1,3 +1,4 @@
+import 'package:firstFlutterApp/model/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuizApp extends StatefulWidget {
@@ -6,6 +7,18 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  int _questionCounter = 0;
+  List questionBank = [
+    Question.name(
+        "The Battle of the Bulge occurred during the First World War?", false),
+    Question.name(
+        "One of the ancient wonders of the world was once located at the Port of Alexandria?",
+        true),
+    Question.name("King Henry VIII was 55 years old when he died?", true),
+    Question.name("The Korean War began in 1940?", false),
+    Question.name(
+        "The Greek God 'Zeus' was given the Roman name 'Jupiter'?", true)
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,22 +27,77 @@ class _QuizAppState extends State<QuizApp> {
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
       ),
-      backgroundColor: Colors.blueGrey.shade100,
+      backgroundColor: Colors.blueGrey.shade300,
       body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              "images/flag.png",
-              height: 200,
-              width: 350,
+            Center(
+              child: Image.asset(
+                "images/flag.png",
+                height: 200,
+                width: 350,
+              ),
             ),
-            Row(),
+            Padding(
+              padding: const EdgeInsets.all(9.0),
+              child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                          color: Colors.grey.shade400,
+                          style: BorderStyle.solid)),
+                  height: 120.0,
+                  child: Center(
+                      child: Text(
+                    questionBank.elementAt(_questionCounter).questionText,
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ))),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  onPressed: () => _checkAnswer(),
+                  color: Colors.blueGrey,
+                  child: Text(
+                    "TRUE",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () => _checkAnswer(),
+                  color: Colors.blueGrey,
+                  child: Text(
+                    "FALSE",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () => _nextQuestion(),
+                  color: Colors.blueGrey,
+                  child: Icon(Icons.arrow_forward, color: Colors.white),
+                )
+              ],
+            ),
+            Spacer(),
             Row()
           ],
         ),
       ),
     );
+  }
+
+  _checkAnswer() {}
+
+  _nextQuestion() {
+    setState(() {
+      _questionCounter = (_questionCounter + 1) % questionBank.length;
+    });
+    print(_questionCounter);
+    print(questionBank.length);
+    print(2 % 5);
   }
 }
