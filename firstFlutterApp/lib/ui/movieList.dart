fc3a5b8c9adfdcebf2717ecb41a7cab81fc2af9c
thelovesmith <averydante9@gmail.com>
2021-05-06 21:32:25 -0400

    constructed the list of movies based on the data from the json file. manually created the Movie class and imported its data

--- firstFlutterApp/lib/ui/movieList.dart
+++ firstFlutterApp/lib/ui/movieList.dart
@@ -1,9 +1,9 @@
-import 'dart:io';
-
+import 'package:firstFlutterApp/model/movies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class MovieListView extends StatelessWidget {
+  final List<Movie> movieList = Movie.getMovies();
   final List movies = [
     "Titanic",
     "Blade Runner",
@@ -16,7 +16,8 @@ class MovieListView extends StatelessWidget {
     "Interstellar",
     "Game Of Thrones",
     "Vikings",
-    "Soul"
+    "Soul",
+    "Benjamin Button",
   ];
   @override
   Widget build(BuildContext context) {
@@ -27,19 +28,64 @@ class MovieListView extends StatelessWidget {
       ),
       backgroundColor: Colors.blueGrey.shade400,
       body: ListView.builder(
-        itemCount: movies.length,
+        itemCount: movieList.length,
         itemBuilder: (BuildContext context, int index) {
           return Padding(
             padding: const EdgeInsets.all(3.0),
             child: Card(
               color: Colors.white,
               child: ListTile(
-                title: Text(movies[index]),
+                leading: CircleAvatar(
+                  child: Container(
+                      decoration:
+                          BoxDecoration(borderRadius: BorderRadius.circular(0)),
+                      child: Text("H")),
+                ),
+                trailing: Text(movieList[index].imdbRating),
+                title: Text(movieList[index].title),
+                subtitle: Text(movieList[index].rated),
+                // onTap: () =>
+                //     debugPrint("Movie name: ${movies.elementAt(index)}"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MovieListViewDetails(
+                                movieName: movieList.elementAt(index).title,
+                              )));
+                },
               ),
-              elevation: 3.0,
+              elevation: 6.0,
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class MovieListViewDetails extends StatelessWidget {
+  final String movieName;
+
+  const MovieListViewDetails({Key key, this.movieName}) : super(key: key);
+// curly braces around this.movie name and key makes them optional when instanciating the statelesswidget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${this.movieName}'),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      body: Center(
+        child: Container(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(
+                  context); // sends you back to the previous route using the already sent context
+            },
+            child: Text("GO Back"),
+          ),
+        ),
       ),
     );
   }
